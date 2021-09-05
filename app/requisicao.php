14ef404f19affc711a37da809c979acda603e25d
Jonatas Miler <jonatasmiler@gmail.com>
2021-09-05 01:57:43 -0300

    teste

--- app/requisicao.php
+++ app/requisicao.php
@@ -15,7 +15,7 @@ class Requisicao
     private $_metodo;
     private $_argumentos;
     private $_modules;
-    
+
     public function __construct()
     {
         if (isset($_GET['url'])) {
@@ -26,21 +26,21 @@ class Requisicao
             $url = explode('/', $url);
             $this->_navLinks = $url;
             $url = array_filter($url);
-            
+
             // separa os blocos da url separados por barra
             $this->_urlAtual = implode('/', $url);
             // lista os modulos da pasta modulos para verificação se o modulo passado
             // no url realmente existe dentro da pasta
-            foreach (new DirectoryIterator(RAIZ."Modulos") as $Files) {
+            foreach (new DirectoryIterator(RAIZ . "modulos") as $Files) {
                 if ($Files->isDir() && $Files->isDot()) {
                     continue;
                 }
                 $this->_modules[] = $Files->getFilename();
             }
-            
+
             // o último bloco do url é o módulo que queremos chamar
             $this->_modulo = strtolower(array_shift($url));
-            
+
             if (!$this->_modulo) {
                 $this->_modulo = false;
             } else {
@@ -50,7 +50,7 @@ class Requisicao
                         $this->_modulo = false;
                     } else {
                         $this->_controlador = strtolower(array_shift($url));
-                        
+
                         if (!$this->_controlador) {
                             $this->_controlador = 'index';
                         }
@@ -60,43 +60,43 @@ class Requisicao
                     $this->_modulo = false;
                 }
             }
-            
+
             $this->_metodo = strtolower(array_shift($url));
             $this->_argumentos = $url;
         }
-        
+
         if (!$this->_controlador) {
             $this->_controlador = CONTROLE_PATRAO;
         }
-        
+
         if (!$this->_metodo) {
             $this->_metodo = 'index';
         }
-        
+
         if (!isset($this->_argumentos)) {
             $this->_argumentos = array();
         }
-        
+
         /*echo "<pre>";
         print_r($this);
         echo "</pre>";*/
     }
-    
+
     public function getControlador()
     {
         return $this->_controlador;
     }
-    
+
     public function getModulo()
     {
         return $this->_modulo;
     }
-    
+
     public function getMetodo()
     {
         return $this->_metodo;
     }
-    
+
     public function getArgs()
     {
         return $this->_argumentos;
